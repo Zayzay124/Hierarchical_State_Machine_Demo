673b818f042540e7b777d7f39809045f85a91e35
Isaiah Rettig <isaiah.rettig@icloud.com>
2025-09-17 00:23:32 -0400

    Player Movement
    - Simple player controller
    -

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -2,15 +2,37 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    private float xInput;
+    private float yInput;
+    private Vector2 inputDir;
 
-    void Start()
-    {
+    public float startSpeed;
+    public float maxSpeed;
 
-    }
 
+    public Rigidbody2D rb;
 
     void Update()
     {
+        xInput = Input.GetAxisRaw("Horizontal");
+        yInput = Input.GetAxisRaw("Vertical");
+
+        inputDir = new Vector2(xInput, yInput).normalized;
+    }
+
+    void FixedUpdate()
+    {
+
+        if (inputDir == Vector2.zero)
+        {
+            rb.linearVelocity = Vector2.zero;
+        }
+        else
+        { // the start up is mostly unnoticable
+            rb.linearVelocityX += xInput * startSpeed;
+            rb.linearVelocityY += yInput * startSpeed;
+            rb.linearVelocity = Vector2.ClampMagnitude(rb.linearVelocity, maxSpeed);
+        }
 
     }
 }
